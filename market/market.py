42a6bdefd1b9104bfcd0b49c7633977950f48283
swchen96 <swchen@mit.edu>
2016-01-08 14:00:47 -0500

    improved cart by using flask request form

--- market/market.py
+++ market/market.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 Things = json.load(open("things.json", "r"))
 cartItems = []
+totalCost = 0
 
 app = Flask("Market")
 
@@ -21,34 +22,37 @@ def index():
                            allThingIDs=sorted(Things.keys()),
                            Things=Things)
 
-@app.route('/thing/<thingID>')
+@app.route('/thing/<thingID>', methods = ['GET','POST'])
 def thing(thingID=None):
-  sendJSON = request.args.get('json', False)
+  if (request.method == 'GET'):
+    sendJSON = request.args.get('json', False)
 
-  if (thingID is None) or (not thingID in Things):
-    if sendJSON:
-      return ('no such Thing: %s' % thingID, 404)
-    else:
-      return(render_template('noSuchThing.html', thingID=thingID), 404)
+    if (thingID is None) or (not thingID in Things):
+      if sendJSON:
+        return ('no such Thing: %s' % thingID, 404)
+      else:
+        return(render_template('noSuchThing.html', thingID=thingID), 404)
   
-  if sendJSON:
-    return jsonify(Things[thingID])
-  else:
-    return render_template('thing.html', thingID=thingID, thingInfo=Things[thingID])
-
-@app.route('/cart/<thingID>')
-def cart(thingID):
-  if (thingID == "VIEW_CART"):
-    cartMessage = "" 
-  else:
-    cartMessage = "Item could not added."
+    if sendJSON:
+      return jsonify(Things[thingID])
+    else:
+      return render_template('thing.html', thingID=thingID, thingInfo=Things[thingID])
+  elif (request.method == 'POST'):
+    cartMessage = "Item could not be added."
+    global totalCost
     if (cartItems.count(thingID) == 0 and (thingID in Things)):
       cartItems.append(thingID)
       cartMessage = "Item added successfully!"
-  totalCost = 0
-  for thingID in cartItems:
-    totalCost += Things[thingID]['price']
-  return render_template('cart.html', cartItems=cartItems, cartMessage=cartMessage, Things=Things, totalCost=totalCost)
+      totalCost += Things[thingID]['price']
+    return render_template('cart.html', cartItems=cartItems, cartMessage=cartMessage, Things=Things, totalCost=totalCost)
+  else:
+    pass
+
+
+@app.route('/cart')
+def cart():
+  global totalCost
+  return render_template('cart.html', cartItems=cartItems, cartMessage="", Things=Things, totalCost=totalCost)
 
 @app.errorhandler(404)
 def page_not_found(e):
