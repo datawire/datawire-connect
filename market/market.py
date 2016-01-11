923dd40a4a6be27766848804cc2e73717c9554e7
swchen96 <swchen@mit.edu>
2016-01-11 14:43:23 -0500

    Moved cart to index page, added ratings and prices, improved look

--- market/market.py
+++ market/market.py
@@ -7,20 +7,38 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 Things = json.load(open("things.json", "r"))
 cartItems = []
-totalCost = 0
 
 app = Flask("Market")
 
+def cartCost():
+  totalCost = 0
+  for thingID in cartItems:
+    totalCost += Things[thingID]['price']
+  return totalCost
+
+def checkout():
+  global cartItems
+  del cartItems[:]
+  return renderIndex("Succesfully checked out your items!")
+  
+
+def renderIndex(cartMessage):
+  return render_template('index.html', 
+                             allThingIDs=sorted(Things.keys()),
+                             Things=Things, cartItems=sorted(cartItems), cartMessage=cartMessage, totalCost=cartCost(), numItems=len(cartItems))
+
 @app.route('/')
 def index():
-  sendJSON = request.args.get('json', False)
+  global cartItems
+  if request.method == 'GET':
+    sendJSON = request.args.get('json', False)
 
-  if sendJSON:
-    return jsonify({ "thingIDs": sorted(Things.keys()) })
-  else:
-    return render_template('index.html', 
-                           allThingIDs=sorted(Things.keys()),
-                           Things=Things)
+    if sendJSON:
+      return jsonify({ "thingIDs": sorted(Things.keys()) })
+    else:
+      return renderIndex("")
+  elif request.method == 'POST' and 'checkout' in request.form:
+    return checkout()
 
 @app.route('/thing/<thingID>', methods = ['GET','POST'])
 def thing(thingID=None):
@@ -37,22 +55,15 @@ def thing(thingID=None):
       return jsonify(Things[thingID])
     else:
       return render_template('thing.html', thingID=thingID, thingInfo=Things[thingID])
-  elif (request.method == 'POST'):
+  elif (request.method == 'POST' and 'addcart' in request.form):
     cartMessage = "Item could not be added."
     global totalCost
     if (cartItems.count(thingID) == 0 and (thingID in Things)):
       cartItems.append(thingID)
       cartMessage = "Item added successfully!"
-      totalCost += Things[thingID]['price']
-    return render_template('cart.html', cartItems=cartItems, cartMessage=cartMessage, Things=Things, totalCost=totalCost)
-  else:
-    pass
-
-
-@app.route('/cart/')
-def cart():
-  global totalCost
-  return render_template('cart.html', cartItems=cartItems, cartMessage="", Things=Things, totalCost=totalCost)
+    return renderIndex(cartMessage)
+  elif request.method == 'POST' and 'checkout' in request.form:
+    return checkout()
 
 @app.errorhandler(404)
 def page_not_found(e):
