9231709a5eb9c158089192a02932c956ff0ab568
Flynn <flynn@datawire.io>
2016-02-16 14:03:28 -0500

    market.py has no ratings. market-ratings.py has (synchronous on refresh) ratings.

--- market/market-ratings.py
+++ market/market-ratings.py
@@ -1,15 +1,19 @@
 import sys
 
 import json
+import os
 import time
 
 from flask import Flask, render_template, url_for, request, jsonify
+from ratings import *
 
 # Start by loading up our set of Things...
 Things = json.load(open("things.json", "r"))
 
 # ...an empty shopping cart...
 cartItems = []
+
+ratings = RatingsClient("ratings", MyHub())
 
 app = Flask("Market")
 
@@ -24,8 +28,38 @@ def checkout():
   del cartItems[:]
   return renderIndex("Succesfully checked out your items!")
 
+def checkRatings(things):
+  wireables = []
+
+  start = os.times()[4]
+
+  for thingID in things:
+    print("ASK for %s" % thingID)      
+    wireables.append(ratings.get(thingID))
+
+  [ wireable.await(1000) for wireable in wireables ]
+
+  end = os.times()[4]
+  elapsed = (end - start) * 1000
+
+  print("WAITED %dms" % elapsed)
+
+  for wireable in wireables:
+    if wireable:
+      thingID = wireable.thingID
+      errString = wireable.getError()
+
+      if errString:
+        print("BAD %s: %s" % (thingID, errString))
+      else:
+        rating = float(wireable.rating) / 10
+        things[thingID]["rating"] = rating
+
+        print("GOT %s: %s" % (thingID, rating))
 
 def renderIndex(cartMessage):
+  checkRatings(Things)
+
   return render_template('index.html',
                          allThingIDs=sorted(Things.keys()),
                          Things=Things,
